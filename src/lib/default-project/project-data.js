138e337cff7e0a4b613bd7b5ff43b5bbcf84ade9
Tong Huazhong <jsyzthz@gmail.com>
2025-05-28 17:31:06 +0800

    更新默认文件。

--- src/lib/default-project/project-data.js
+++ src/lib/default-project/project-data.js
@@ -2,10 +2,10 @@ import {defineMessages} from 'react-intl';
 import sharedMessages from '../shared-messages';
 
 let messages = defineMessages({
-    chirp: {
-        defaultMessage: 'Chirp',
-        description: 'Name for the chirp sound',
-        id: 'gui.defaultProject.chirp'
+    meow: {
+        defaultMessage: 'Meow',
+        description: 'Name for the meow sound',
+        id: 'gui.defaultProject.meow'
     },
     variable: {
         defaultMessage: 'my variable',
@@ -69,33 +69,24 @@ const projectData = translateFunction => {
                 currentCostume: 0,
                 costumes: [
                     {
-                        assetId: '5de48e72637db4d7fc7f6bb526354114',
+                        assetId: 'b7853f557e4426412e64bb3da6531a00',
                         name: translator(messages.costume, {index: 1}),
                         bitmapResolution: 1,
-                        md5ext: '5de48e72637db4d7fc7f6bb526354114.svg',
+                        md5ext: 'b7853f557e4426412e64bb3da6531a00.svg',
                         dataFormat: 'svg',
-                        rotationCenterX: 93,
-                        rotationCenterY: 80
-                    },
-                    {
-                        assetId: '48bbc82a10af3d0e0b7d5c677097df0e',
-                        name: translator(messages.costume, {index: 2}),
-                        bitmapResolution: 1,
-                        md5ext: '48bbc82a10af3d0e0b7d5c677097df0e.svg',
-                        dataFormat: 'svg',
-                        rotationCenterX: 93,
-                        rotationCenterY: 80
+                        rotationCenterX: 48,
+                        rotationCenterY: 50
                     }
                 ],
                 sounds: [
                     {
-                        assetId: '4e080acae1c3dc65eef30f07c2b0d4a5',
-                        name: translator(messages.chirp),
+                        assetId: '83c36d806dc92327b9e7049a565c6bff',
+                        name: translator(messages.meow),
                         dataFormat: 'wav',
                         format: '',
                         rate: 22050,
                         sampleCount: 18688,
-                        md5ext: '4e080acae1c3dc65eef30f07c2b0d4a5.wav'
+                        md5ext: '83c36d806dc92327b9e7049a565c6bff.wav'
                     }
                 ],
                 volume: 100,
